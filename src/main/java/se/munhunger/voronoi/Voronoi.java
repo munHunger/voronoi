54b2428c2b2d3749826221bf7af635af46722634
munHunger <marcusmunger@hotmail.com>
2017-11-13 19:42:10 +0100

    Showing 2 images in the window

--- src/main/java/se/munhunger/voronoi/Voronoi.java
+++ src/main/java/se/munhunger/voronoi/Voronoi.java
@@ -1,13 +1,22 @@
 package se.munhunger.voronoi;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * @author Marcus Münger
  */
 public class Voronoi {
-    public static void main(String[] args) {
+    public static BufferedImage image;
+    public static BufferedImage paintedImage;
+
+    public static void main(String[] args) throws IOException {
+        image = ImageIO.read(Voronoi.class.getClassLoader().getResource("ref1.jpg"));
+        paintedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+
         JFrame frame = new JFrame("Voronoi painter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1600, 800);
@@ -16,15 +25,16 @@ public class Voronoi {
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         frame.add(panel);
 
-        JPanel images = new JPanel() {
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                super.setBackground(Color.PINK);
-            }
-        };
+        JPanel images = new JPanel();
+        images.setLayout(new BoxLayout(images, BoxLayout.LINE_AXIS));
+        JLabel ref = new JLabel(new ImageIcon(image));
+        JLabel result = new JLabel(new ImageIcon(paintedImage));
+        images.add(result);
+        images.add(ref);
 
         panel.add(images);
 
+        frame.pack();
         frame.setVisible(true);
     }
 }
